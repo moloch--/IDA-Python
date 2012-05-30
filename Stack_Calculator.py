cf5ea968b646bee3acf7e0bf48d17db3ba9aa229
Moloch <joedemesy@gmail.com>
2012-05-30 01:13:57 -0700

    Added a GUI to Stack Calc

--- Stack_Calculator.py
+++ Stack_Calculator.py
@@ -1,20 +1,22 @@
 ###################################################
-# Stack Variable Calculator
-# By Moloch - v0.1
+# Stack Variable Calculator - v0.2
+# About: Automatically finds stack variables and 
+#        calculates their size in bytes
+# By Moloch
 ###################################################
 from idaapi import *
 
 # Min buffer size to display
-BUFFER_SIZE  = 16
+BUFFER_SIZE  = 0
 
 def stack_calc(function):
 	''' Calculates stack variable sizes for a given function '''
 	stack_frame = GetFrame(function) 
 	frame_size = GetStrucSize(stack_frame)
-	display_title = True
 	frame_counter = 0
 	flag = -1
 	
+	print "\n[*] Function:", GetFunctionName(function)
 	while frame_counter < frame_size:
 		stack_var = GetMemberName(stack_frame, frame_counter)
 		if stack_var != None:
@@ -22,33 +24,55 @@ def stack_calc(function):
 				size = frame_counter - flag
 				if stack_var == " s":
 					size -= 8
-					frame_counter = frame_size
+					frame_counter += 8
 				if BUFFER_SIZE < size:
-					if display_title:
-						display_title = False
-						print "\n[*] Function:", GetFunctionName(function)
-					print "\t%s (%d bytes)" % (current_member, size)
+					print "   Stack -> %s (%d bytes)" % (current_member, size)
 				flag = frame_counter
 				current_member = stack_var
-				try:
-					frame_counter += GetMemberSize(stack_frame, frame_counter)
-				except:
-					frame_counter += 1
 			else:
 				flag = frame_counter
 				current_member = stack_var
-				try:
-					frame_counter += GetMemberSize(stack_frame, frame_counter)
-				except:
-					frame_counter += 1
+			try:
+				frame_counter += GetMemberSize(stack_frame, frame_counter)
+			except:
+				frame_counter += 1
 		else:
 			frame_counter += 1
-###################################################
-# > Start	
-###################################################
-current_address = ScreenEA()
-print "\n\n\n*** Stack Variable Calculator - v0.1 ***"
-for function in Functions(SegStart(current_address), SegEnd(current_address)):
-	stack_calc(function)
-		
 
+### GUI Class
+class StackCalculator(Choose2):
+
+    def __init__(self, title):
+        Choose2.__init__(self, title, [ ["Address", 10 | Choose2.CHCOL_HEX], ["Name", 30 | Choose2.CHCOL_PLAIN] ])
+        self.title = title
+        self.n = 0
+        self.icon = 41
+        self.PopulateItems()
+
+    def PopulateItems(self):
+        self.items = [ [hex(x), GetFunctionName(x), x] for x in idautils.Functions() ]
+        
+    def OnClose(self):
+        print "[*] Closed ", self.title
+
+    def OnSelectLine(self, index):
+        stack_calc(self.items[index][2])
+
+    def OnGetLine(self, index):
+        return self.items[index]
+
+    def OnGetSize(self):
+        return len(self.items)
+
+    def OnDeleteLine(self, index):
+        ea = self.items[index][2]
+        idc.Delfunc_name(ea)
+        return index
+
+    def OnRefresh(self, index):
+        self.PopulateItems()
+        return index
+
+### Start GUI
+gui = StackCalculator("Stack Calculator - v0.2")
+gui.Show()
